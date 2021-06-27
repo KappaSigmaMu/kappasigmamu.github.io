875db1b74546e6eb8e91f10ade418b755233547d
Alan R. Fachini <hi@alfakini.com>
2021-06-27 16:26:41 -0300

    Add bootstrap and react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,33 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-import './App.css';
 import About from './pages/About';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <p>KappaSigmaMu Society</p>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
+    <Container>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand as={Link} to="/">KappaSigmaMu Society</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
 
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
-
-      </div>
-    </Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Container>
   );
 }
 
