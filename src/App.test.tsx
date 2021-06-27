44fa43bb3d1002d7c9e23bf948f0dac3e1301a71
Alan R. Fachini <hi@alfakini.com>
2021-06-27 15:48:11 -0300

    Add react-router-dom

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/KappaSigmaMu Society/i);
+  expect(element).toBeInTheDocument();
 });
---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+export default About;
