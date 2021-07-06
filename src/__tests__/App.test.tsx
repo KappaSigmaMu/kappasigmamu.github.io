8d5a5a9aaa32ca58b8de070c6e2646c9d3cdb3d1
murilooon <murilooon@gmail.com>
2021-07-06 17:39:21 -0300

    fix: test import

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import App from '../App'
+import { App } from '../App'
 
 test('renders App', () => {
   render(
