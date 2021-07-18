a8768ba53ed5cc7edc10897d2545e24eb83099d0
murilooon <murilooon@gmail.com>
2021-07-18 17:56:08 -0300

    feat: connect and show members

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { Switch, Route, Link } from 'react-router-dom'
+import { KusamaContextProvider, useKusama } from './kusama-lib'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 
-function App() {
+function Main() {
+  const { apiState, apiError } = useKusama()
+
+  const loader = (text: string) => {
+    return <p>{text}</p>
+  }
+
+  if (apiState === 'ERROR')
+    return loader(`${JSON.stringify(apiError, null, 4)}`)
+  if (apiState !== 'READY') return loader('Connecting')
+
   return (
     <Container fluid className="px-0">
       <Navbar bg="dark" variant="dark">
@@ -33,6 +44,14 @@ function App() {
         </Route>
       </Switch>
     </Container>
+  )
+}
+
+function App() {
+  return (
+    <KusamaContextProvider>
+      <Main />
+    </KusamaContextProvider>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react'
+import { useKusama } from '../kusama-lib'
+
 function Home() {
+  const { api } = useKusama()
+  const [members, setMembers] = useState([])
+
+  useEffect(() => {
+    api.derive.society.members().then((response: any) => {
+      setMembers(response)
+    })
+  }, [])
+
   return (
     <div>
       <h2>Home</h2>
+      {members.map((member: any) => (
+        <p key={member.accountId}>{member.accountId.toHuman()}</p>
+      ))}
     </div>
   )
 }
