3dbcff7a27d4f2f104a2c7a7b403bc904cfdce85
Alan R. Fachini <hi@alfakini.com>
2021-07-25 14:36:53 -0300

    Improve configs

--- src/kusama-lib/config/index.ts
+++ src/kusama-lib/config/index.ts
@@ -1,12 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-import configCommon from './common.json'
-import types from './types.json'
-
 const configEnv = require(`./${process.env.NODE_ENV}.json`)
-
-const envVarNames = ['PROVIDER_SOCKET', 'DEVELOPMENT_KEYRING']
-
-const config = { ...configCommon, ...configEnv, ...envVarNames, types }
+const config = { ...configEnv }
 
 export { config }
