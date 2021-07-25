5287a44aac62c0215e386900fd7709520c16d317
murilooon <murilooon@gmail.com>
2021-07-25 15:28:47 -0300

    fix: cypress

--- cypress/integration/1-getting-started/navbar.ts
+++ cypress/integration/1-getting-started/navbar.ts
@@ -1,8 +1,11 @@
 describe('NavBar', () => {
   it('should render navbar', () => {
-    cy.get('[alt="KappaSigmaMu Society canary brand"]').should('exist')
-    cy.contains('Home').should('have.attr', 'href')
+    cy.get('[alt="KappaSigmaMu"]').should('exist')
+
+    cy.contains('Kusama Network').should('have.attr', 'href')
     cy.contains('About').should('have.attr', 'href').and('include', 'about')
+    cy.contains('Blog').should('have.attr', 'href')
+    cy.contains('Cyborg Journey').should('have.attr', 'href')
 
     cy.contains('About').click()
     cy.location('pathname').should('eq', `/about`)
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,4 +2,5 @@ import './commands'
 
 beforeEach(() => {
   cy.visit('/')
+  cy.wait(5000)
 })
