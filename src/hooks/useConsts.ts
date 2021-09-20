bd998e20c78f6cdf7c291e0b0ef29b59f38dffb5
Alan R. Fachini <hi@alfakini.com>
2021-09-20 10:16:01 -0300

    WIP: Add CurrentRoundRow (#22)
    * Add react-circular-progressbar package
    * Add component CurrentRoundRow
    * Add @polkadot packages
    * Get current round data from rpc api
    * Get currentAccount strikes from RPC API
    * Add useConsts hook to memo society consts from rpc api
    * Temporarily add CurrentRoundRow component to Home page
    
    Co-authored-by: Lauro Gripa <laurogripa@gmail.com>

---
+++ src/hooks/useConsts.ts
@@ -0,0 +1,43 @@
+// @ts-nocheck
+// Copied from: https://github.com/polkadot-js/apps/blob/master/packages/react-hooks/src/useBlockTime.ts
+
+import type { BalanceOf, BlockNumber, u32, PalletId } from '@polkadot/api'
+import { useMemo } from 'react'
+import { useSubstrate } from '../substrate'
+
+type SocietyConsts = {
+  candidateDeposit: BalanceOf
+  challengePeriod: BlockNumber
+  maxCandidateIntake: u32
+  maxStrikes: u32
+  palletId: PalletId
+  periodSpend: BalanceOf
+  rotationPeriod: BlockNumber
+  wrongSideDeduction: BalanceOf
+}
+
+export function useConsts(): SocietyConsts {
+  const { api } = useSubstrate()
+
+  return useMemo((): SocietyConsts => {
+    const candidateDeposit: BalanceOf = api.consts.society.candidateDeposit
+    const challengePeriod: BlockNumber = api.consts.society.challengePeriod
+    const maxCandidateIntake: u32 = api.consts.society.maxCandidateIntake
+    const maxStrikes: u32 = api.consts.society.maxStrikes
+    const palletId: PalletId = api.consts.society.palletId
+    const periodSpend: BalanceOf = api.consts.society.periodSpend
+    const rotationPeriod: BlockNumber = api.consts.society.rotationPeriod
+    const wrongSideDeduction: BalanceOf = api.consts.society.wrongSideDeduction
+
+    return {
+      candidateDeposit,
+      challengePeriod,
+      maxCandidateIntake,
+      maxStrikes,
+      palletId,
+      periodSpend,
+      rotationPeriod,
+      wrongSideDeduction,
+    }
+  })
+}
