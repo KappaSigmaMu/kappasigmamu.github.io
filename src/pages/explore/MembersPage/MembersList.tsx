ac2799a4e01c8f5e9e9155452a789bb5a9dab81f
Alan R. Fachini <services@alfakini.com>
2021-11-26 17:12:57 -0300

    Remove strikes, payouts and defender from list

--- src/pages/explore/MembersPage/MembersList.tsx
+++ src/pages/explore/MembersPage/MembersList.tsx
@@ -2,33 +2,35 @@ import Identicon from '@polkadot/react-identicon'
 import { Badge, Col } from 'react-bootstrap'
 import { DataHeaderRow, DataRow } from '../../../components/base'
 import { truncateMiddle } from '../../../helpers/truncate'
-import { PayoutsAccordion } from './components/PayoutsAccordion'
 
 const MembersList = ({ members }: { members: SocietyMember[] }): JSX.Element => {
   return (<>
     <DataHeaderRow>
       <Col xs={1} className="text-center">#</Col>
-      <Col xs={2} className="text-start">Wallet Hash</Col>
-      <Col xs={3} className="text-start">Payouts</Col>
-      <Col xs={6} className="text-end"></Col>
+      <Col xs={3} className="text-start">Wallet Hash</Col>
+      <Col xs={8} className="text-end"></Col>
     </DataHeaderRow>
     {members.map((member: SocietyMember) => (
       <DataRow key={member.accountId.toString()}>
         <Col xs={1} className="text-center">
           <Identicon value={member.accountId} size={32} theme={'polkadot'} />
         </Col>
-        <Col xs={2} className="text-start text-truncate">
+        <Col xs={3} className="text-start text-truncate">
           {truncateMiddle(member.accountId?.toString())}
         </Col>
-        <Col xs={3} className="text-start">
-          {member.hasPayouts ? <PayoutsAccordion payouts={member.payouts} /> : <></>}
-        </Col>
-        <Col xs={6} className="text-end">
-          {member.isFounder ? <Badge pill bg="primary" className="me-2">Founder</Badge> : <></>}
-          {member.isHead ? <Badge pill bg="primary" className="me-2">Head</Badge> : <></>}
-          {member.isDefenderVoter ? <Badge pill bg="primary" className="me-2">Defender</Badge> : <></>}
-          {member.hasStrikes ? <Badge pill bg="danger" className="me-2">{member.strikesCount} Strikes</Badge> : <></>}
-          {member.isSkeptic ? <Badge pill bg="danger" className="me-2">Skeptic</Badge> : <></>}
+        <Col xs={8} className="text-end">
+          {member.isFounder
+            ? <Badge bg="secondary" text="dark" className="me-2">society founder</Badge>
+            : <></>
+          }
+          {member.isHead
+            ? <Badge bg="primary" className="me-2">society head</Badge>
+            : <></>
+          }
+          {member.isSkeptic
+            ? <Badge bg="danger" className="me-2">round skeptic</Badge>
+            : <></>
+          }
         </Col>
       </DataRow>
     ))}
