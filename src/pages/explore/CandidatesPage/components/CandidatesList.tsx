882b274ff955ebdbdf0bfe2037bed2564758ae91
Eduardo Fonseca <edrd.fons@gmail.com>
2022-06-02 21:19:18 -0300

    Add bid/vouch info to suspended list (#97)
    Also improve some candidate list elements to make it
    more user-friendly and standard with other pages.

--- src/pages/explore/CandidatesPage/components/CandidatesList.tsx
+++ src/pages/explore/CandidatesPage/components/CandidatesList.tsx
@@ -2,7 +2,7 @@ import Identicon from '@polkadot/react-identicon'
 import { Col } from 'react-bootstrap'
 import { DataHeaderRow, DataRow } from '../../../../components/base'
 import { FormatBalance } from '../../../../components/FormatBalance'
-import { truncateMiddle } from '../../../../helpers/truncate'
+import { truncate, truncateMiddle } from '../../../../helpers/truncate'
 
 const CandidatesList = ({ candidates }: { candidates: SocietyCandidate[] }): JSX.Element => {
   if (candidates.length === 0) return (
@@ -11,7 +11,7 @@ const CandidatesList = ({ candidates }: { candidates: SocietyCandidate[] }): JSX
   
   return (<>
     <DataHeaderRow>
-      <Col xs={1} className="text-start">#</Col>
+      <Col xs={1} className="text-center">#</Col>
       <Col xs={3} className="text-start">Wallet Hash</Col>
       <Col className="text-start">Bid Kind</Col>
       <Col></Col>
@@ -20,24 +20,24 @@ const CandidatesList = ({ candidates }: { candidates: SocietyCandidate[] }): JSX
     
     {candidates.map((candidate: SocietyCandidate) => (
       <DataRow key={candidate.accountId.toString()}>
-        <Col xs={1} className="text-start">
+        <Col xs={1} className="text-center">
           <Identicon value={candidate.accountId} size={32} theme={'polkadot'} />
         </Col>
         <Col xs={3} className="text-start text-truncate">
           {truncateMiddle(candidate.accountId?.toString())}
         </Col>
-        <Col>
+        <Col xs={1}>
           {candidate.kind.isDeposit ? 'Deposit' : 'Vouch'}
         </Col>
-        <Col className="text-start">
+        <Col xs={4} className="text-start">
           {candidate.kind.isDeposit
             ? <FormatBalance balance={candidate.value} />
-            : (<p>
-                Vouching Member: {candidate.kind.asVouch[0].toHuman()} -
-                Vouching Tip: {candidate.kind.asVouch[1].toHuman()}
-               </p>)}
+            : (<>
+                Member: {truncate(candidate.kind.asVouch[0].toHuman(), 7)} |
+                Tip: {<FormatBalance balance={candidate.kind.asVouch[1]}></FormatBalance>}
+               </>)}
         </Col>
-        <Col>
+        <Col xs={3}>
           Skeptics
         </Col>
       </DataRow>))}
--- src/pages/explore/SuspendedPage/components/SuspendedList.tsx
+++ src/pages/explore/SuspendedPage/components/SuspendedList.tsx
@@ -1,19 +1,17 @@
 import Identicon from '@polkadot/react-identicon'
 import { AccountId } from '@polkadot/types/interfaces'
-import { arrayFlatten } from '@polkadot/util'
 import { Col, Badge } from 'react-bootstrap'
 import { DataHeaderRow, DataRow } from '../../../../components/base'
-import { truncateMiddle } from '../../../../helpers/truncate'
+import { FormatBalance } from '../../../../components/FormatBalance'
+import { truncate, truncateMiddle } from '../../../../helpers/truncate'
 
 type SuspendedListProps = { 
-  candidates: AccountId[]
+  candidates: SuspendedCandidate[]
   members: AccountId[] 
 }
 
 const SuspendedList = ({ candidates, members }: SuspendedListProps): JSX.Element => {
-  const accountIds = arrayFlatten([members, candidates])
-
-  if (accountIds.length === 0) return (
+  if (candidates.length === 0 && members.length === 0) return (
     <>No suspended members or candidates</>
   )
   
@@ -21,26 +19,49 @@ const SuspendedList = ({ candidates, members }: SuspendedListProps): JSX.Element
     <DataHeaderRow>
       <Col xs={1} className="text-center">#</Col>
       <Col xs={3} className="text-start">Wallet Hash</Col>
-      <Col xs={8} className="text-end"></Col>
+      <Col xs={8} className="text-start">Bid Kind</Col>
     </DataHeaderRow>
-    {accountIds.map((accountId: AccountId) => {
-      const isMember = members.indexOf(accountId) >= 0
-      return (
-        <DataRow key={accountId.toString()}>
-          <Col xs={1} className="text-center">
-            <Identicon value={accountId} size={32} theme="polkadot" />
-          </Col>
-          <Col xs={3} className="text-start text-truncate">
-            {truncateMiddle(accountId.toString())}
-          </Col>
-          <Col xs={8} className="text-end">
-            <Badge pill bg={isMember ? "primary" : "dark"} className="me-2 p-2">
-              {isMember ? <>Member</> : <>Candidate</>}
-            </Badge>
-          </Col>
-        </DataRow>
-      )
-    })}
+    {members.map((accountId: AccountId) => (
+      <DataRow key={accountId.toString()}>
+        <Col xs={1} className="text-center">
+          <Identicon value={accountId} size={32} theme="polkadot" />
+        </Col>
+        <Col xs={3} className="text-start text-truncate">
+          {truncateMiddle(accountId.toString())}
+        </Col>
+        <Col xs={8} className="text-end">
+          <Badge pill bg="primary" className="me-2 p-2">
+            Member
+          </Badge>
+        </Col>
+      </DataRow>
+    ))}
+    {candidates.map((candidate: SuspendedCandidate) => (
+      <DataRow key={candidate.accountId.toString()}>
+      <Col xs={1} className="text-center">
+        <Identicon value={candidate.accountId} size={32} theme="polkadot" />
+      </Col>
+      <Col xs={3} className="text-start text-truncate">
+        {truncateMiddle(candidate.accountId.toString())}
+      </Col>
+      <Col xs={1}>
+        {candidate.bid.isDeposit ? <>Deposit</> : <>Vouch</>}
+      </Col>
+      <Col xs={4}>
+        {candidate.bid.isDeposit 
+          ? <FormatBalance balance={candidate.balance} /> 
+          : (<>
+              Member: {truncate(candidate.bid.asVouch[0].toHuman(), 7)} |
+              Tip: {<FormatBalance balance={candidate.bid.asVouch[1]}></FormatBalance>}
+             </>)}
+      </Col>
+      <Col xs={3} className="text-end">
+        <Badge pill bg="dark" className="me-2 p-2">
+          Candidate
+        </Badge>
+      </Col>
+    </DataRow>
+    ))}
   </>)
 }
 
