2669a968647cfda34f09c85f47440b782193fff0
Lauro Gripa <laurogripa@gmail.com>
2023-12-16 01:12:47 -0300

    Add candidate identity

--- src/pages/explore/CandidatesPage/components/CandidatesList.tsx
+++ src/pages/explore/CandidatesPage/components/CandidatesList.tsx
@@ -6,17 +6,18 @@ import { useEffect, useRef, useState } from 'react'
 import { Button, Col } from 'react-bootstrap'
 import { CandidateDetailsOffcanvas } from './CandidateDetailsOffcanvas'
 import { VoteButton } from './VoteButton'
+import { AccountIdentity } from '../../../../components/AccountIdentity'
 import { DataHeaderRow, DataRow } from '../../../../components/base'
 import { FormatBalance } from '../../../../components/FormatBalance'
-import { truncate, truncateMiddle } from '../../../../helpers/truncate'
+import { truncate } from '../../../../helpers/truncate'
 import ApproveIcon from '../../../../static/approve-icon.svg'
 import CheckAllIcon from '../../../../static/check-all-icon.svg'
 import RejectIcon from '../../../../static/reject-icon.svg'
 import { StyledAlert } from '../../components/StyledAlert'
 
 type CandidatesListProps = {
-  api: ApiPromise,
-  activeAccount: accountType,
+  api: ApiPromise
+  activeAccount: accountType
   candidates: SocietyCandidate[]
 }
 
@@ -26,7 +27,7 @@ type VoteResult = {
 }
 
 const AlreadyVotedIcon = () => (
- <>
+  <>
     <img src={CheckAllIcon} className="me-2" />
     <label style={{ color: '#6c757d' }}>Voted</label>
   </>
@@ -34,14 +35,24 @@ const AlreadyVotedIcon = () => (
 
 // TODO: only let members vote
 // TODO: check error/success messages (non-members votes are returning success)
-const CandidatesList = ({ api, activeAccount, candidates }: CandidatesListProps): JSX.Element => {
+const CandidatesList = ({
+  api,
+  activeAccount,
+  candidates
+}: CandidatesListProps): JSX.Element => {
   const [showAlert, setShowAlert] = useState(false)
   const [votes, setVotes] = useState<SocietyCandidate[]>([])
-  const [voteResult, setVoteResult] = useState<VoteResult>({ success: false, message: '' })
+  const [voteResult, setVoteResult] = useState<VoteResult>({
+    success: false,
+    message: ''
+  })
   const society = api?.query?.society
 
-  const [selectedCandidate, setSelectedCandidate] = useState<AccountId | null>(null)
-  const [showCandidateDetailsOffcanvas, setShowCandidateDetailsOffcanvas] = useState(false)
+  const [selectedCandidate, setSelectedCandidate] = useState<AccountId | null>(
+    null
+  )
+  const [showCandidateDetailsOffcanvas, setShowCandidateDetailsOffcanvas] =
+    useState(false)
   const showMessage = (result: VoteResult) => {
     setVoteResult(result)
     setShowAlert(true)
@@ -61,14 +72,18 @@ const CandidatesList = ({ api, activeAccount, candidates }: CandidatesListProps)
     if (candidates.length === 0) return
 
     candidates.forEach((candidate) => {
-      society.votes(candidate.accountId, activeAccount.address, (vote: Option<SocietyVote>) => {
-        if (vote.isEmpty) {
-          if (prevActiveAccount != activeAccount) setVotes([])
-          return
-        }
+      society.votes(
+        candidate.accountId,
+        activeAccount.address,
+        (vote: Option<SocietyVote>) => {
+          if (vote.isEmpty) {
+            if (prevActiveAccount != activeAccount) setVotes([])
+            return
+          }
 
-        setVotes([candidate.accountId, ...votes])
-      })
+          setVotes([candidate.accountId, ...votes])
+        }
+      )
     })
   }, [activeAccount, prevActiveAccount])
 
@@ -77,67 +92,88 @@ const CandidatesList = ({ api, activeAccount, candidates }: CandidatesListProps)
     setShowCandidateDetailsOffcanvas(true)
   }
 
-  if (candidates.length === 0) return (
-    <>No candidates</>
-  )
+  if (candidates.length === 0) return <>No candidates</>
 
-  return (<>
-    {selectedCandidate &&
-      <CandidateDetailsOffcanvas
-        api={api}
-        candidateId={selectedCandidate}
-        show={showCandidateDetailsOffcanvas}
-        onClose={() => setShowCandidateDetailsOffcanvas(false)} />}
+  return (
+    <>
+      {selectedCandidate && (
+        <CandidateDetailsOffcanvas
+          api={api}
+          candidateId={selectedCandidate}
+          show={showCandidateDetailsOffcanvas}
+          onClose={() => setShowCandidateDetailsOffcanvas(false)}
+        />
+      )}
 
-    <StyledAlert
-      success={voteResult.success}
-      onClose={() => setShowAlert(false)}
-      show={showAlert}
-      dismissible>
-      {voteResult.message}
-    </StyledAlert>
+      <StyledAlert
+        success={voteResult.success}
+        onClose={() => setShowAlert(false)}
+        show={showAlert}
+        dismissible
+      >
+        {voteResult.message}
+      </StyledAlert>
 
-    <DataHeaderRow>
-      <Col xs={1} className="text-center">#</Col>
-      <Col xs={3} className="text-start">Wallet Hash</Col>
-      <Col className="text-start">Bid Kind</Col>
-      <Col></Col>
-      <Col>Vote</Col>
-    </DataHeaderRow>
-
-    {candidates.map((candidate: SocietyCandidate) => (
-      <DataRow key={candidate.accountId.toString()}>
+      <DataHeaderRow>
         <Col xs={1} className="text-center">
-          <Identicon value={candidate.accountId} size={32} theme={'polkadot'} />
+          #
         </Col>
-        <Col xs={3} className="text-start text-truncate">
-          {truncateMiddle(candidate.accountId?.toString())}
+        <Col xs={3} className="text-start">
+          Wallet Hash
         </Col>
-        <Col xs={1}>
-          {candidate.kind.isDeposit ? 'Deposit' : 'Vouch'}
-        </Col>
-        <Col xs={2} className="text-start">
-          {candidate.kind.isDeposit
-            ? <FormatBalance balance={candidate.value} />
-            : (<>
-              Member: {truncate(candidate.kind.asVouch[0].toHuman(), 7)} |
-              Tip: {<FormatBalance balance={candidate.kind.asVouch[1]}></FormatBalance>}
-            </>)}
-        </Col>
-        <Col xs={2} onClick={() => showCandidateDetails(candidate.accountId)}>
-          <Button variant="link">Votes</Button>
-        </Col>
-        <Col xs={3}>
-          {votes.includes(candidate.accountId)
-            ? <AlreadyVotedIcon />
-            : <>
+        <Col className="text-start">Bid Kind</Col>
+        <Col></Col>
+        <Col>Vote</Col>
+      </DataHeaderRow>
+
+      {candidates.map((candidate: SocietyCandidate) => (
+        <DataRow key={candidate.accountId.toString()}>
+          <Col xs={1} className="text-center">
+            <Identicon
+              value={candidate.accountId}
+              size={32}
+              theme={'polkadot'}
+            />
+          </Col>
+          <Col xs={3} className="text-start text-truncate">
+            <AccountIdentity api={api} accountId={candidate.accountId} />
+          </Col>
+          <Col xs={1}>{candidate.kind.isDeposit ? 'Deposit' : 'Vouch'}</Col>
+          <Col xs={2} className="text-start">
+            {candidate.kind.isDeposit ? (
+              <FormatBalance balance={candidate.value} />
+            ) : (
+              <>
+                Member: {truncate(candidate.kind.asVouch[0].toHuman(), 7)} |
+                Tip:{' '}
+                {
+                  <FormatBalance
+                    balance={candidate.kind.asVouch[1]}
+                  ></FormatBalance>
+                }
+              </>
+            )}
+          </Col>
+          <Col xs={2} onClick={() => showCandidateDetails(candidate.accountId)}>
+            <Button variant="link">Votes</Button>
+          </Col>
+          <Col xs={3}>
+            {votes.includes(candidate.accountId) ? (
+              <AlreadyVotedIcon />
+            ) : (
+              <>
                 <VoteButton
                   api={api}
                   showMessage={showMessage}
                   successText="Approval vote sent."
                   waitingText="Approval vote request sent. Waiting for response..."
-                  vote={{ approve: true, voterAccount: activeAccount, candidateId: candidate.accountId }}
-                  icon={ApproveIcon}>
+                  vote={{
+                    approve: true,
+                    voterAccount: activeAccount,
+                    candidateId: candidate.accountId
+                  }}
+                  icon={ApproveIcon}
+                >
                   <u>Approve</u>
                 </VoteButton>
                 <VoteButton
@@ -145,15 +181,22 @@ const CandidatesList = ({ api, activeAccount, candidates }: CandidatesListProps)
                   showMessage={showMessage}
                   successText="Rejection vote sent."
                   waitingText="Rejection vote request sent. Waiting for response..."
-                  vote={{ approve: false, voterAccount: activeAccount, candidateId: candidate.accountId }}
-                  icon={RejectIcon}>
+                  vote={{
+                    approve: false,
+                    voterAccount: activeAccount,
+                    candidateId: candidate.accountId
+                  }}
+                  icon={RejectIcon}
+                >
                   <u>Reject</u>
                 </VoteButton>
               </>
-          }
-        </Col>
-      </DataRow>))}
-  </>)
+            )}
+          </Col>
+        </DataRow>
+      ))}
+    </>
+  )
 }
 
 export { CandidatesList }
