c221103cbf8453b46ba443df8d4dbc07216bbba6
Lauro Gripa <laurogripa@gmail.com>
2023-12-18 01:34:05 -0300

    Fix spinner size

--- src/pages/explore/components/LoadingSpinner.tsx
+++ src/pages/explore/components/LoadingSpinner.tsx
@@ -2,8 +2,13 @@ import { Spinner } from 'react-bootstrap'
 
 type LoadingSpinnerProps = {
   center?: boolean
+  small?: boolean
 }
 
-export function LoadingSpinner({ center = true }: LoadingSpinnerProps): JSX.Element {
-  return <Spinner animation="border" variant="primary" className={center ? 'mx-auto d-block' : ''} />
+export function LoadingSpinner({ center = true, small = false }: LoadingSpinnerProps): JSX.Element {
+  return small ? (
+    <Spinner animation="border" variant="primary" className={center ? 'mx-auto d-block' : ''} size="sm" />
+  ) : (
+    <Spinner animation="border" variant="primary" className={center ? 'mx-auto d-block' : ''} />
+  )
 }
