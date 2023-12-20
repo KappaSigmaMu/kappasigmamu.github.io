156707b7d4ea9fc7394ce0b52630254c38043209
Lauro Gripa <laurogripa@gmail.com>
2023-12-20 03:18:35 -0300

    Add buttons to vote for the defender

--- src/pages/explore/helpers.ts
+++ src/pages/explore/helpers.ts
@@ -1,24 +1,21 @@
 import type { ApiPromise } from '@polkadot/api'
-import { DeriveSocietyMember } from '@polkadot/api-derive/types'
 import { StorageKey, u32 } from '@polkadot/types'
 import type { AccountId, AccountId32 } from '@polkadot/types/interfaces'
 import { BN } from '@polkadot/util'
 import { combineLatest, firstValueFrom, map, of } from 'rxjs'
 
 function buildSocietyCandidatesArray(response: any): SocietyCandidate[] {
-  return response.map((item: any) => (
-    {
-      accountId: item[0].args[0] as AccountId,
-      kind: item[1].unwrap().kind,
-      bid: item[1].unwrap().bid,
-      tally: item[1].unwrap().tally,
-      skepticStruck: item[1].unwrap().skepticStruck,
-    }
-  ))
+  return response.map((item: any) => ({
+    accountId: item[0].args[0] as AccountId,
+    kind: item[1].unwrap().kind,
+    bid: item[1].unwrap().bid,
+    tally: item[1].unwrap().tally,
+    skepticStruck: item[1].unwrap().skepticStruck
+  }))
 }
 
 const buildSocietyMembersArray = (
-  members: DeriveSocietyMember[],
+  members: ExtendedDeriveSociety[],
   info: ExtendedDeriveSociety | null,
   graceStrikes: BN
 ): SocietyMember[] => {
@@ -37,36 +34,36 @@ const buildSocietyMembersArray = (
     strikes: member.strikes,
     strikesCount: member.strikes.isEmpty ? 0 : member.strikes.toNumber(),
     vouching: member.vouching,
-    vote: member.vote
+    vote: member.vote,
+    rank: member.rank
   }))
 
   return membersArray.sort(sortSocietyMembersArray)
 }
 
-const sortSocietyMembersArray = (a: SocietyMember, b: SocietyMember): number =>
-  a.isDefender !== b.isDefender
-    ? a.isDefender
-      ? -1
-      : 1
-    : a.isSkeptic !== b.isSkeptic
-      ? a.isSkeptic
-        ? -1
-        : 1
-      : a.isHead !== b.isHead
-        ? a.isHead
-          ? -1
-          : 1
-        : a.isFounder !== b.isFounder
-          ? a.isFounder
-            ? -1
-            : 1
-          : a.isDefenderVoter !== b.isDefenderVoter
-            ? a.isDefenderVoter
-              ? -1
-              : 1
-            : 1
+const sortSocietyMembersArray = (a: SocietyMember, b: SocietyMember): number => {
+  if (a.isDefender !== b.isDefender) {
+    return a.isDefender ? -1 : 1
+  }
+  if (a.isSkeptic !== b.isSkeptic) {
+    return a.isSkeptic ? -1 : 1
+  }
+  if (a.isHead !== b.isHead) {
+    return a.isHead ? -1 : 1
+  }
+  if (a.isFounder !== b.isFounder) {
+    return a.isFounder ? -1 : 1
+  }
+  if (a.rank.toNumber() > 0 !== b.rank.toNumber() > 0) {
+    return a.rank.toNumber() > 0 ? -1 : 1
+  }
+  if (a.isDefenderVoter !== b.isDefenderVoter) {
+    return a.isDefenderVoter ? -1 : 1
+  }
+  return 0
+}
 
-async function deriveMembersInfo(api: ApiPromise): Promise<DeriveSocietyMember[]> {
+async function deriveMembersInfo(api: ApiPromise): Promise<ExtendedDeriveSociety[]> {
   const currentChallengeRound: u32 = (await api.query.society.challengeRoundCount()) as u32
 
   const memberKeys = (await api.query.society.members.keys()) as StorageKey<[AccountId32]>[]
@@ -86,7 +83,7 @@ async function deriveMembersInfo(api: ApiPromise): Promise<DeriveSocietyMember[]
             members[index].isSome
               ? {
                   accountId,
-                  isDefenderVoter: defenderVotes[index].isSome ? defenderVotes[index].unwrap().approve.isTrue : false,
+                  isDefenderVoter: defenderVotes[index].isSome,
                   isSuspended: suspendedMembers[index].isSome,
                   member: members[index].unwrap(),
                   payouts: payouts[index].payouts
@@ -100,7 +97,8 @@ async function deriveMembersInfo(api: ApiPromise): Promise<DeriveSocietyMember[]
             isSuspended,
             payouts,
             strikes: member.strikes,
-            vouching: member.vouching.unwrapOr(undefined)
+            vouching: member.vouching.unwrapOr(undefined),
+            rank: member.rank
           }))
       )
     )
