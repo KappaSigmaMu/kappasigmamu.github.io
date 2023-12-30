cf079a66878eb4c665d06fbb53647b12efd6b5a0
Lauro Gripa <laurogripa@gmail.com>
2023-12-29 21:40:06 -0300

    Add rules page

--- src/components/LinkWithQuery.tsx
+++ src/components/LinkWithQuery.tsx
@@ -1,13 +1,25 @@
 import { Link, LinkProps, useLocation } from 'react-router-dom'
+import styled from 'styled-components'
 
 const LinkWithQuery = ({ children, to, ...props }: LinkProps & React.RefAttributes<HTMLAnchorElement>) => {
   const { search } = useLocation()
+  const isSelected = location.pathname === to.toString() || location.pathname === to.toString() + '/'
 
   return (
-    <Link to={to + search} {...props}>
+    <StyledLink to={to + search} {...props} selected={isSelected}>
       {children}
-    </Link>
+    </StyledLink>
   )
 }
+
+interface StyledLinkProps extends LinkProps {
+  selected?: boolean
+}
+
+const StyledLink = styled(Link)<StyledLinkProps>`
+  color: ${(props) => props.selected && 'white !important'};
+  pointer-events: ${(props) => (props.selected ? 'none' : 'auto')};
+  cursor: ${(props) => (props.selected ? 'default' : 'pointer')};
+`
 
 export { LinkWithQuery }
