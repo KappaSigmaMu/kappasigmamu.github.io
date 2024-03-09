90c9025d01ca3957add4d58910f16a4a6c091d4d
Lauro Gripa <laurogripa@gmail.com>
2024-03-08 23:05:43 -0300

    Add verification badge

--- src/helpers/humanize.ts
+++ src/helpers/humanize.ts
@@ -1,12 +1,11 @@
 import type { PalletSocietyBidKind } from '@polkadot/types/lookup'
-import { truncate } from './truncate'
 
 function humanizeBidKind(bid: Partial<PalletSocietyBidKind>) {
   if (bid.isDeposit) {
     return 'Deposit'
   } else if (bid.isVouch) {
     const accountId = bid.asVouch?.[0] || ''
-    return `Vouch: ${truncate(accountId.toString())}`
+    return `Vouch: ${accountId.toString()}`
   } else {
     return 'Unknown'
   }
