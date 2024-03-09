650f37a26e2636b96c588c36c8aa451df2ef6c9e
Lauro Gripa <laurogripa@gmail.com>
2024-03-08 23:05:43 -0300

    Add verification badge

--- src/components/AccountIdentity.tsx
+++ src/components/AccountIdentity.tsx
@@ -2,6 +2,8 @@ import type { ApiPromise } from '@polkadot/api'
 import type { DeriveAccountRegistration } from '@polkadot/api-derive/types'
 import { AccountId } from '@polkadot/types/interfaces'
 import { useEffect, useState } from 'react'
+import { FaCircleCheck, FaCircleMinus } from 'react-icons/fa6'
+import { styled } from 'styled-components'
 import { truncateMiddle } from '../helpers/truncate'
 
 const AccountIdentity = ({
@@ -14,16 +16,42 @@ const AccountIdentity = ({
   hideNotSet?: boolean
 }) => {
   const [id, setId] = useState<string>('')
+  const [verified, setVerified] = useState(false)
 
   useEffect(() => {
     api.derive.accounts.identity(accountId, (identity: DeriveAccountRegistration) => {
       identity.display && setId(identity.display)
+      identity.judgements.length > 0 && setVerified(true)
     })
   }, [])
 
+  const verifiedBadge = verified ? <StyledVerifiedBadge className="me-2" /> : <StyledUnverifiedBadge className="me-2" />
+
   if (!id && hideNotSet) return <></>
-  if (id) return <>{id}</>
+  if (id)
+    return (
+      <div className="d-flex align-items-center justify-content-center justify-content-lg-start">
+        {verifiedBadge}
+        {id}
+      </div>
+    )
   return <>{truncateMiddle(accountId?.toString(), 20)}</>
 }
+
+const StyledVerifiedBadge = styled(FaCircleCheck)`
+  flex-shrink: 0;
+
+  & path {
+    fill: ${(props) => props.theme.colors.secondary};
+  }
+`
+
+const StyledUnverifiedBadge = styled(FaCircleMinus)`
+  flex-shrink: 0;
+
+  & path {
+    fill: ${(props) => props.theme.colors.lightGrey};
+  }
+`
 
 export { AccountIdentity }
