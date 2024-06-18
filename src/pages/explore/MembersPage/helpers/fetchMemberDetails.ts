87468cc7a25a2564ca33d1d8960531d69a38eba0
Lauro Gripa <laurogripa@gmail.com>
2024-06-18 15:24:39 -0300

    Update polkadot packages

--- src/pages/explore/MembersPage/helpers/fetchMemberDetails.ts
+++ src/pages/explore/MembersPage/helpers/fetchMemberDetails.ts
@@ -1,9 +1,9 @@
 import { ApiPromise } from '@polkadot/api'
-import { Data } from '@polkadot/types'
+// import { Data } from '@polkadot/types'
 import { AccountId } from '@polkadot/types/interfaces'
 import '@polkadot/api-augment/substrate'
-import { PalletIdentityIdentityInfo } from '@polkadot/types/lookup'
-import { u8aToBuffer } from '@polkadot/util'
+// import { PalletIdentityLegacyIdentityInfo } from '@polkadot/types/lookup'
+// import { u8aToBuffer } from '@polkadot/util'
 
 export async function fetchMemberDetails(api: ApiPromise, accountId: AccountId): Promise<SocietyMemberDetails> {
   const [accountInfo, maybeIdentity] = await Promise.all([
@@ -11,7 +11,7 @@ export async function fetchMemberDetails(api: ApiPromise, accountId: AccountId):
     api.query.identity.identityOf(accountId)
   ])
 
-  const identity = maybeIdentity.isSome ? buildAccountIdentity(maybeIdentity.unwrap().info) : undefined
+  const identity = maybeIdentity.isSome ? undefined : undefined // temporary until we integrate people's chain
 
   const rawIndex = accountInfo?.accountIndex
   const index = rawIndex ? api.registry.createType('AccountIndex', rawIndex.toNumber()).toString() : undefined
@@ -19,15 +19,15 @@ export async function fetchMemberDetails(api: ApiPromise, accountId: AccountId):
   return { accountId, identity, index }
 }
 
-function buildAccountIdentity(identityInfo: PalletIdentityIdentityInfo): AccountIdentity {
-  return {
-    name: decode(identityInfo.display) ?? '(Unable to get name)',
-    email: decode(identityInfo.email),
-    legal: decode(identityInfo.legal),
-    riot: decode(identityInfo.riot),
-    twitter: decode(identityInfo.twitter),
-    webpage: decode(identityInfo.web)
-  }
-}
+// function buildAccountIdentity(identityInfo: PalletIdentityLegacyIdentityInfo): AccountIdentity {
+//   return {
+//     name: decode(identityInfo.display) ?? '(Unable to get name)',
+//     email: decode(identityInfo.email),
+//     legal: decode(identityInfo.legal),
+//     riot: decode(identityInfo.riot),
+//     twitter: decode(identityInfo.twitter),
+//     webpage: decode(identityInfo.web)
+//   }
+// }
 
-const decode = (data: Data) => (data.isEmpty ? undefined : u8aToBuffer(data.toU8a()).toString())
+// const decode = (data: Data) => (data.isEmpty ? undefined : u8aToBuffer(data.toU8a()).toString())
