da6222ac38c826a192492601b7664cc5ebaf5261
Lauro Gripa <laurogripa@gmail.com>
2025-10-08 18:41:20 -0300

    Change RPC providers from Relay to Asset Hub

--- src/helpers/providers.ts
+++ src/helpers/providers.ts
@@ -1,18 +1,15 @@
 export type Provider = { name: string; url: string; dev: boolean }
 
 export const providers = [
-  { name: 'Blockops', url: 'wss://kusama-public-rpc.blockops.network/ws', dev: false },
-  { name: 'Dwellir', url: 'wss://kusama-rpc.dwellir.com', dev: false },
-  { name: 'Dwellir Tunisia', url: 'wss://kusama-rpc-tn.dwellir.com', dev: false },
-  { name: 'Geometry', url: 'wss://kusama.geometry.io/websockets', dev: false },
-  { name: 'IBP', url: 'wss://rpc.ibp.network/kusama', dev: false },
-  { name: 'IBP Dotters', url: 'wss://rpc.dotters.network/kusama', dev: false },
-  { name: 'LuckyFriday', url: 'wss://rpxc-kusama.luckyfriday.io', dev: false },
-  { name: 'OnFinality', url: 'wss://kusama.api.onfinality.io/public-ws', dev: false },
-  { name: 'Parity', url: 'wss://kusama-rpc.polkadot.io', dev: false },
-  { name: 'RadiumBlock', url: 'wss://kusama.public.curie.radiumblock.co/ws', dev: false },
-  { name: 'StakeWorld', url: 'wss://ksm-rpc.stakeworld.io', dev: false },
-  { name: '1RPC', url: 'wss://1rpc.io/ksm', dev: false },
+  { name: 'Dwellir', url: 'wss://asset-hub-kusama-rpc.n.dwellir.com', dev: false },
+  { name: 'Dwellir Tunisia', url: 'wss://statemine-rpc-tn.dwellir.com', dev: false },
+  { name: 'IBP1', url: 'wss://sys.ibp.network/asset-hub-kusama', dev: false },
+  { name: 'IBP2', url: 'wss://asset-hub-kusama.dotters.network', dev: false },
+  { name: 'LuckyFriday', url: 'wss://rpc-asset-hub-kusama.luckyfriday.io', dev: false },
+  { name: 'OnFinality', url: 'wss://assethub-kusama.api.onfinality.io/public-ws', dev: false },
+  { name: 'Parity', url: 'wss://kusama-asset-hub-rpc.polkadot.io', dev: false },
+  { name: 'RadiumBlock', url: 'wss://statemine.public.curie.radiumblock.co/ws', dev: false },
+  { name: 'Stakeworld', url: 'wss://ksm-rpc.stakeworld.io/assethub', dev: false },
   { name: 'Local', url: 'ws://127.0.0.1:9444', dev: true },
   { name: 'Chopsticks', url: 'ws://127.0.0.1:8000', dev: true }
 ]
