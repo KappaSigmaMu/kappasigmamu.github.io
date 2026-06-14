f25f7c9a6224ccd2dabc70a212402c6ce48386fe
Lauro Gripa <laurogripa@gmail.com>
2026-06-14 15:13:50 -0300

    Create grep command for e2e tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,5 +1,8 @@
 import '@chainsafe/cypress-polkadot-wallet'
+import registerCypressGrep from '@cypress/grep/src/support'
 import './commands'
+
+registerCypressGrep()
 
 Cypress.Commands.add('getBySel', (selector: string, ...args: any[]) => {
   return cy.get(`[data-test="${selector}"]`, ...args)
---
+++ scripts/test-e2e-grep.js
@@ -0,0 +1,20 @@
+const { spawnSync } = require('child_process')
+
+const grep = process.argv.slice(2).join(' ').trim()
+
+if (!grep) {
+  console.error('Usage: yarn test:e2e:grep "<test title substring>"')
+  process.exit(1)
+}
+
+const cypress = 'cypress run --config video=false --env grepFilterSpecs=true,grepOmitFiltered=true'
+const withApp = `start-server-and-test start:test:ready http://localhost:3000 "${cypress}"`
+const command = `start-server-and-test chopsticks http://localhost:8000 '${withApp}'`
+
+const result = spawnSync(command, {
+  stdio: 'inherit',
+  shell: true,
+  env: { ...process.env, CYPRESS_grep: grep },
+})
+
+process.exit(result.status ?? 1)
